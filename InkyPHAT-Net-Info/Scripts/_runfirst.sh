d4dea130f3634b6a6c75c72ba51c8a651d5147cf
madtvmaster90210 <69994411+madtvmaster90210@users.noreply.github.com>
2020-12-16 12:56:35 -0500

    Update _runfirst.sh

--- InkyPHAT-Net-Info/Scripts/_runfirst.sh
+++ InkyPHAT-Net-Info/Scripts/_runfirst.sh
@@ -2,5 +2,5 @@
 ##The following are the dependencies for the Pi0 W running the lite Raspbian OS
 
 
-sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip git libatlas-base-dev python3-numpy libopenjp2-7 libtiff5 && pip3 install inky[rpi,fonts] && pip3 install pillow && crontab -l | { cat; echo "@reboot python3 /home/pi/inky/Scripts/command.py.test &"; } | crontab -
+sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip git libatlas-base-dev python3-numpy libopenjp2-7 libtiff5 && pip3 install inky[rpi,fonts] && pip3 install pillow && sudo apt-get install daemontools daemontools-run && git clone https://github.com/madtvmaster90210/inky.git && sudo mkdir /etc/service/butto
 
