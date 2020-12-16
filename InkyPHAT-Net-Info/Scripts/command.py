9f1e10e943ebfe1183d5b67cd2f19ca7f89c1f20
madtvmaster90210 <69994411+madtvmaster90210@users.noreply.github.com>
2020-12-15 19:12:48 -0500

    Update command.py

--- InkyPHAT-Net-Info/Scripts/command.py
+++ InkyPHAT-Net-Info/Scripts/command.py
@@ -6,15 +6,15 @@ import subprocess
 import runpy
 
 
-#wlan = subprocess.call("/home/pi/inky/Scripts/wlannet.py", shell=True)
+#wlan = subprocess.call("/home/pi/inky/InkyPHAT-Net-Info/Scripts/wlannet.py", shell=True)
 #eth = subprocess.run("/home/pi/inky/Scripts/ethnet.py").read().strip()
 
 @buttonshim.on_press(buttonshim.BUTTON_A)
 def handler(button, pressed):
-	runpy.run_path(path_name='/home/pi/inky/Scripts/wlannet.py')
+	runpy.run_path(path_name='/home/pi/inky/InkyPHAT-Net-Info/Scripts/wlannet.py')
 
 @buttonshim.on_press(buttonshim.BUTTON_B)
 def handler(button, pressed):
-	runpy.run_path(path_name='/home/pi/inky/Scripts/ethnet.py')
+	runpy.run_path(path_name='/home/pi/inky/InkyPHAT-Net-Info/Scripts/ethnet.py')
 
 signal.pause()
