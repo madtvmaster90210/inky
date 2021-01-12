60dbec92388a65532317cc962d9ac3736c3ad719
madtvmaster90210 <69994411+madtvmaster90210@users.noreply.github.com>
2021-01-12 10:31:30 -0500

    Create command.py

---
+++ InkyPHAT-Net-Info/red/Scripts/command.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+import signal
+import buttonshim
+import subprocess
+import runpy
+
+
+#wlan = subprocess.call("/home/pi/inky/InkyPHAT-Net-Info/Scripts/wlannet.py", shell=True)
+#eth = subprocess.run("/home/pi/inky/Scripts/ethnet.py").read().strip()
+
+@buttonshim.on_press(buttonshim.BUTTON_A)
+def handler(button, pressed):
+	runpy.run_path(path_name='/home/pi/inky/InkyPHAT-Net-Info/Scripts/wlannet.py')
+
+@buttonshim.on_press(buttonshim.BUTTON_B)
+def handler(button, pressed):
+	runpy.run_path(path_name='/home/pi/inky/InkyPHAT-Net-Info/Scripts/ethnet.py')
+
+signal.pause()
