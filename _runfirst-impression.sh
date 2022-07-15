81e5b474b70494957cbc728507f9cc45e634f3b8
madtvmaster90210 <69994411+madtvmaster90210@users.noreply.github.com>
2022-07-15 12:43:23 -0400

    Update and rename _runfirst-wbuttons.sh to _runfirst-impression.sh

---
+++ _runfirst-impression.sh
@@ -0,0 +1,7 @@
+##While creating the scripts for the Pi 0 W, I failed to test for requirements when running on the lite version of raspbianOS.
+##The following are the dependencies for the Pi0 W running the lite Raspbian OS
+
+
+sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip git libatlas-base-dev python3-numpy libopenjp2-7 libtiff5 python3-pil && pip3 install inky==1.3.1 && pip3 install rpi.gpio && pip3 install python-resize-image && pip3 install font_source_serif_pro && pip3 install font_source_sans_pro
+
+
