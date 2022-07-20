873a264a675aabfb58f2d7418ca53b42cd9fefdf
madtvmaster90210 <69994411+madtvmaster90210@users.noreply.github.com>
2022-07-20 12:43:10 -0400

    Update _runfirst-impression.sh

--- _runfirst-impression.sh
+++ _runfirst-impression.sh
@@ -2,6 +2,6 @@
 ##The following are the dependencies for the Pi0 W running the lite Raspbian OS
 
 
-sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip git libatlas-base-dev python3-numpy libopenjp2-7 libtiff5 python3-pil && pip3 install inky==1.3.1 && pip3 install rpi.gpio && pip3 install python-resize-image && pip3 install font_source_serif_pro && pip3 install font_source_sans_pro && pip3 install pyowm
+sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip git libatlas-base-dev python3-numpy libopenjp2-7 libtiff5 python3-pil && pip3 install inky==1.3.1 && pip3 install rpi.gpio && pip3 install python-resize-image && pip3 install font_source_serif_pro && pip3 install font_source_sans_pro && pip3 install pyowm && pip3 install speedtest-cli
 
 
