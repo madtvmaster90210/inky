fa842b92b89f1ef710d8af727bd8db3a057479ad
madtvmaster90210 <69994411+madtvmaster90210@users.noreply.github.com>
2022-07-20 12:49:57 -0400

    Update _runfirst-impression.sh

--- _runfirst-impression.sh
+++ _runfirst-impression.sh
@@ -1,7 +1,21 @@
+#!/bin/bash
 ##While creating the scripts for the Pi 0 W, I failed to test for requirements when running on the lite version of raspbianOS.
 ##The following are the dependencies for the Pi0 W running the lite Raspbian OS
+if [ `whoami` != root ]; then
+    echo '!!!!Run as sudo!!!!!'
+    exit
+fi
 
-
-sudo apt update && sudo apt upgrade -y && sudo apt install python3-pip git libatlas-base-dev python3-numpy libopenjp2-7 libtiff5 python3-pil && pip3 install inky==1.3.1 && pip3 install rpi.gpio && pip3 install python-resize-image && pip3 install font_source_serif_pro && pip3 install font_source_sans_pro && pip3 install pyowm && pip3 install speedtest-cli
+sudo apt update && sudo apt upgrade -y;
+sudo apt install python3-pip \
+    git \
+    libatlas-base-dev \
+    python3-numpy \
+    libopenjp2-7 \
+    libtiff5 \
+    python3-pil;
+pip3 install inky==1.3.1 rpi.gpio python-resize-image font_source_serif_pro font_source_sans_pro pyowm speedtest-cli;
+sudo echo -e '\ndtparam=i2c_arm=on\ndtparam=spi=on' >> /boot/config.txt;
+sudo reboot
 
 
